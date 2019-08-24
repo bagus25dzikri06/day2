d08609d5260393949d622a8d4044bee5d45d6b83
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-08-24 13:39:57 +0700

    The 2nd task is done

--- exercise-2.js
+++ exercise-2.js
@@ -1,1 +1,8 @@
-Akses Nilai Array
+//Mengakses Nilai dalam Suatu Array
+function balikString(kataKalimat) {
+   var strTerbalik = ''
+   for (var angka = kataKalimat.length - 1; angka >= 0; angka--) {
+      strTerbalik =  strTerbalik + kataKalimat[angka]
+   }
+   console.log(strTerbalik)
+}
