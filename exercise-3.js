44e6e8d62b874168f2db986a47cc5f7fc32ccb26
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-08-25 15:21:17 +0700

    The code has been completed

--- exercise-3.js
+++ exercise-3.js
@@ -1,4 +1,15 @@
 /* Melakukan Looping Data Array */
-function dataHandling() {
-
+function dataHandling(biodata) {
+   for (var i = 0; i < biodata.length; i++) {
+      for (var j = 0; i < biodata.length; i++) {
+         console.log('Nomor ID: ' + biodata[i][j])
+         j += 1
+         console.log('Nama Lengkap: ' + biodata[i][j])
+         j += 2
+         console.log('TTL: ' + biodata[i][j] + biodata[i][j + 1])
+         j += 1
+         console.log('Hobi: ' + biodata[i][j])
+      }
+      console.log(' ')
+   }
 }
