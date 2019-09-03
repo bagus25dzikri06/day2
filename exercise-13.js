8fed6946415b8fd78192a9a277e9fce7e4ae7735
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-09-04 00:12:34 +0700

    Program mencari jarak target terdekat sukses dijalankan

--- exercise-13.js
+++ exercise-13.js
@@ -1,1 +1,10 @@
-Mengamati deret geometri
+//Mencari jarak target terdekat
+function targetTerdekat(arr) {
+   var jarak = arr.indexOf('x') - arr.indexOf('y')
+
+   if (jarak > 0) {
+      return jarak
+   } else {
+      return jarak + (arr.length - 1)
+   }
+}
